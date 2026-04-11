463e0230d917be89130536bf7ee8fa3e18829a6f
nguyennanhcd <anh487303@gmail.com>
2026-04-11 18:00:34 +0700

    feat: done refactoring

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { AuthCookieService } from './auth-cookie.service';
+import { AuthCookieService } from './services/auth-cookie.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
