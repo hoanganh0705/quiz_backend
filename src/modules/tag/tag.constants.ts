7b6bf7cc8a41c93b4d388538caee63ee4e564b9c
nguyennanhcd <anh487303@gmail.com>
2026-04-23 10:06:12 +0700

    feat: split reusable code

---
+++ src/modules/tag/tag.constants.ts
@@ -0,0 +1,6 @@
+export const TAG_SLUG_EMPTY_MESSAGE = 'Tag slug cannot be empty';
+
+export const TAG_SLUG_INVALID_MESSAGE =
+  'Tag slug must be lowercase and can only contain letters, numbers, and hyphens';
+
+export const TAG_UNIQUE_CONFLICT_MESSAGE = 'Tag name or slug already exists';
