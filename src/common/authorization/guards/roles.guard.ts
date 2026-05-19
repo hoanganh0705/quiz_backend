2f1d86f9031d8efdbf1844d9f81ac85890fe227b
nguyennanhcd <anh487303@gmail.com>
2026-05-19 08:31:17 +0700

    feat: revise the work flow

--- src/common/authorization/guards/roles.guard.ts
+++ src/common/authorization/guards/roles.guard.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { isUserRole, type UserRole } from '@/common/types/user-role.type';
+import { IS_PUBLIC_KEY } from '@/common/decorators/public.decorator';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 import { assertRequestUser } from '../utils/request-user.util';
@@ -26,6 +27,15 @@ export class RolesGuard implements CanActivate {
   }
 
   canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const requiredRoles = this.reflector.getAllAndOverride<unknown>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
--- src/modules/email/email.constants.ts
+++ src/modules/email/email.constants.ts
@@ -1,5 +1,6 @@
 export const EMAIL_QUEUE_NAME = 'email';
 
+// Using Symbols for DI tokens to ensure uniqueness and avoid potential naming collisions
 export const EMAIL_QUEUE_TOKENS = {
   CONNECTION: Symbol('EMAIL_QUEUE_CONNECTION'),
   QUEUE: Symbol('EMAIL_QUEUE'),
