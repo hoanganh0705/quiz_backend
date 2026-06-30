6932f5aa2e82cd313b67f80af35be961f5bb5d30
nguyennanhcd <anh487303@gmail.com>
2026-06-30 22:52:46 +0700

    do nothing

--- src/debug.ts
+++ src/debug.ts
@@ -2,6 +2,8 @@ import { repl } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  await repl(AppModule);
+  await repl(AppModule, {
+    preview: false,
+  });
 }
 void bootstrap();
