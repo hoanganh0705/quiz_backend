c99b4bf1d3a142732b3cbc8dc826e7436acab97e
nguyennanhcd <anh487303@gmail.com>
2026-07-01 08:09:57 +0700

    feat: add seed to user badge and notification

--- src/commands/seed/development/index.ts
+++ src/commands/seed/development/index.ts
@@ -4,3 +4,5 @@ export * from './bookmark.seed';
 export * from './review.seed';
 export * from './discussion.seed';
 export * from './ranking.seed';
+export * from './user-badge.seed';
+export * from './notification.seed';
